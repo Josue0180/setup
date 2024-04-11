4e7c16f3cca2adfc9e3b8e92146d96fe1ba584bb
Josue0180 <Jooficial14@gmail.com>
2024-04-11 11:32:47 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "Josue0180"
+git config --global user.email "Jooficial14@gmail.com"
+
+cd C:\Users\JOSUEDEOLIVEIRAREIS\
+
+git clone https://github.com/Josue0180/Caesar-Cipher.git
+cd Caesar-Cipher
+
+code .
